c7b6b4881737b11fe07bb92426236484fac952b5
Ganchao Wei <ganchao.wei@uconn.edu>
2021-09-18 11:12:50 -0400

    debug NUTS

--- LDS/debug/debug_X3.m
+++ LDS/debug/debug_X3.m
@@ -70,8 +70,6 @@ Q_fit = repmat(Q,1,1,ng); % true
 % priors
 Q0 = eye(nClus*p)*1e-2;
 
-
-
 C_trans_tmp = zeros(N, p*nClus);
 for k = 1:length(Lab)
     C_trans_tmp(k, ((Lab(k)-1)*p+1):(Lab(k)*p)) = C_fit(k,:,1);
@@ -79,15 +77,48 @@ end
 [X_fit(:,:,1),~,~] = ppasmoo_poissexp_v2(Y,C_trans_tmp,d_fit(:,1),...
     x0_fit(:,1),Q0,A_fit(:,:,1),b_fit(:,1),Q_fit(:,:,1));
 
-% X_fit(:,:,1) = zeros(nClus*p, T);
+X_fit(:,:,1) = zeros(nClus*p, T);
 
 %%
-opt.M=1;
-opt.Madapt=0;
-opt.delta = 0.5
 x_norm = zeros(ng, 1);
 x_norm(1) = norm(X_fit(:,:,1), 'fro');
-rej_path = zeros(ng,1);
+
+
+C_trans_tmp = zeros(N, p*nClus);
+for k = 1:N
+    C_trans_tmp(k, ((Lab(k)-1)*p+1):(Lab(k)*p)) = C_fit(k,:,1);
+end
+d_tmp = d_fit(:,1);
+x0_tmp = x0_fit(:,1);
+A_tmp = A_fit(:,:,1);
+b_tmp = b_fit(:,1);
+Q_tmp = Q_fit(:,:,1);
+
+logNPrior = @(X) -1/2*(X(:,1) - x0_tmp)'*inv(Q0)*(X(:,1) - x0_tmp) -...
+    1/2*trace((X(:,2:end) - A_tmp*X(:,1:(end-1)) - b_tmp)'*inv(Q_tmp)*...
+    (X(:,2:end) - A_tmp*X(:,1:(end-1)) - b_tmp));
+
+lamX = @(X) exp(C_trans_tmp*X + d_tmp) ;
+lpdf = @(vecX) sum(log(poisspdf(Y, lamX(reshape(vecX, [], T)))), 'all') +...
+    logNPrior(reshape(vecX, [], T));
+glpdf = @(vecX) derX(vecX, d_tmp, C_trans_tmp, x0_tmp,...
+    Q0, Q_tmp, A_tmp, b_tmp, Y);
+
+fg=@(vecX) deal(lpdf(vecX), glpdf(vecX)); % log density and gradient
+
+
+n_mcmc = 50;
+n_warmup = 10;
+delta = 0.8;
+n_updates = 10;
+n_itr_per_update = ceil(n_mcmc / n_updates);
+
+% Adapt the step-size using dual-averaging algorithm.
+n_updates_warmup = ceil(n_warmup / n_itr_per_update);
+[muXvec_tmp, epsilon] = dualAveraging(fg, reshape(X_fit(:,:,g-1), [], 1),...
+    delta, n_warmup, n_updates_warmup);
+X_fit(:,:,1) = reshape(muXvec_tmp,[], T);
+
 tic;
 for g = 2:50
     
@@ -116,13 +147,11 @@ for g = 2:50
     glpdf = @(vecX) derX(vecX, d_tmp, C_trans_tmp, x0_tmp,...
         Q0, Q_tmp, A_tmp, b_tmp, Y);
     
-    fg=@(vecX_r) deal(lpdf(vecX_r'), glpdf(vecX_r')'); % log density and gradient
+    fg=@(vecX) deal(lpdf(vecX), glpdf(vecX)); % log density and gradient
     
-    [muXvec_NUTS, energies, diagn]=hmc_nuts(fg, reshape(X_fit(:,:,g-1),[],1)',...
-        opt);
-    rej_path(g) = diagn.rej;
-    disp(rej_path(g))
-    X_fit(:,:,g) = reshape(muXvec_NUTS(2,:),[], T);
+    % muXvec_NUTS = ReNUTS(fg, epsilon, reshape(X_fit(:,:,g-1), [], 1));
+    muXvec_NUTS = NUTS(fg, epsilon, reshape(X_fit(:,:,g-1), [], 1));
+    X_fit(:,:,g) = reshape(muXvec_NUTS,[], T);
     
     x_norm(g) = norm(X_fit(:,:,g), 'fro');
     figure(1)
@@ -131,7 +160,26 @@ for g = 2:50
 end
 toc;
 
+%%
 
+
+idx = round(ng/2):ng;
+
+figure
+subplot(3,2,1)
+plot(X(1:p,:)')
+title('true')
+subplot(3,2,2)
+plot(mean(X_fit(1:p,:,idx), 3)')
+title('fit')
+subplot(3,2,3)
+plot(X(p+1:2*p,:)')
+subplot(3,2,4)
+plot(mean(X_fit(p+1:2*p,:,idx), 3)')
+subplot(3,2,5)
+plot(X(2*p+1:3*p,:)')
+subplot(3,2,6)
+plot(mean(X_fit(2*p+1:3*p,:,idx), 3)')
 
 
 
